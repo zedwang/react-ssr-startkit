f30d3ca8bf41ed81b9d2e5953a39e94af59dbc2d
zed <woox.wzd@gmail.com>
2019-05-06 12:04:48 +0800

    fixed error msg when rendered

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from './container';
-
 import createStore from "./store";
-const {store} = createStore( window.REDUX_DATA );
 import './styles/main.scss';
+const {store} = createStore( window.REDUX_DATA );
 
-const app = document.getElementById( "root" );
-
-ReactDOM.hydrate( Root, app );
+const app = document.getElementById("root");
+// resolve warning
+// Warning: Expected server HTML to contain a matching <div> in <div>
+// see:https://github.com/nozzle/react-static/issues/144
+const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
+renderMethod( < Root store = { store } />, app );
 
 if (module.hot) {
     module.hot.accept('./container', () => {
-        ReactDOM.hydrate( <Root store={store}/>, app );
+        renderMethod( < Root store = { store }/>, app );
     });
 }
